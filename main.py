29e3e78a26c5a98fbb2f93dcb1d206db7507fa5d
Centauria <centauria_v_c@163.com>
2020-08-11 17:37:01 +0800

    main

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import argparse
 import base64
+import hashlib
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -9,4 +10,8 @@ if __name__ == '__main__':
     
     with open(args.input) as f:
         b = base64.b64decode(f.read())
-        print(b)
+        print(f'Decode length: {len(b)}')
+        print(f'Last 32 bytes: {b[-32:]}')
+        sh = hashlib.md5()
+        sh.update(b[:-32])
+        print(f'MD5 without last 32 bytes: {sh.hexdigest()}')
